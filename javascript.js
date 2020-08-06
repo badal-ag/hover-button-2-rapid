2d22c901b2f1367068c71bf2e64923506489cab3
Badal Agarwal <badal.agarwal1369@gmail.com>
2020-08-06 22:25:16 +0530

    Rapid Action Button

---
+++ javascript.js
@@ -0,0 +1,15 @@
+var state = false;
+
+function expand() {
+    if(state == false) {
+        document.getElementById('items').style.transform = "scaleX(1)";
+        document.getElementById('toggle').style.transform = "rotate(45deg)";
+        state = true;
+    }
+
+    else {
+        document.getElementById('items').style.transform = "scaleX(0)";
+        document.getElementById('toggle').style.transform = "rotate(0deg)";
+        state = false;
+    }
+}
